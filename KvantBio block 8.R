26e4de005eff77630836dc6bc408eca161d05b48
Rasmus Hammar <Rasmus.Hammar@gmx.com>
2024-03-05 12:44:24 +0100

    block 8 update quiz 1 done

---
+++ KvantBio block 8.R
@@ -0,0 +1,146 @@
+# KvantBio block 8
+
+# Quiz 1 ----
+library(deSolve)
+# dSdt = (1 - (S/P))*c - ((S*m)/P)
+
+# Funktion med ekvationssystem
+island_fun <- function(times, # Intervall
+                       y,     # Begynnelsevärden
+                       parms  # Andra värden
+                       ) {
+  # variabler att använda
+  c <- parms["c"]
+  P <- parms["P"]
+  m <- parms["m"]
+  S <- y["S"]
+  
+  # Differentialekvationen att lösa numeriskt
+  dSdt <- (1 - (S/P))*c - ((S*m)/P)
+  
+  # Spara & returnera resultatet
+  result <- list(
+    dSdt
+  )
+  return(result)
+}
+
+# Vektor med begynnelsevärden
+init_cond <- c(
+  S = 5
+)
+
+# Vektor med andra värden för formeln
+parameters <- c(
+  c = 2,
+  P = 50,
+  m = 4
+)
+
+# Vektor med alla tidssteg (t) att sätta in
+time_span <- seq(
+  from = 0, # Start på intervall
+  to = 100, # Slut på intervall
+  by =   1  # Steglängd
+)
+
+# Lösa ekvationerna numeriskt
+solution <- ode(
+  y = init_cond,      # Begynnelsevärden
+  times = time_span,  # Intervall
+  func = island_fun,  # Funktionen
+  parms = parameters, # Andra värden
+  method = "rk4"      # Runge-Kutta version 4
+)
+# Convertera till data frame för lättare hantering
+sol <- as.data.frame(solution)
+
+# Plot time vs antal arter
+plot(
+  sol$time,
+  sol$S,
+  type = "l",
+  xlab = "Tid",
+  ylab = "Antal arter",
+  ylim = c(0,30),
+  col = "blue"
+)
+# lines(sol$time,
+#       sol$S)
+
+# c = koloniseringshastigheten
+# ökar -> ökar antalet arter vid jämvikt & ökar fortare
+
+# P = antal arter som kan kolonisera ön
+# ökar -> ökar antalet arter vid jämvikt & ökar fortare
+
+# m = utdöende hastighet
+# ökar -> minskar antal arter vid jämvikt & når jämnvikt fortare
+
+# Sista värdet av S
+sista <- tail(sol,
+              n = 1)$S
+sista
+# Hitta sista värdet med uniroot (därför att differentialekvationen redan är en derivata?)
+S_eq <- function(S,
+                 c = 2,
+                 P = 50,
+                 m = 4) {
+  (1 - (S/P))*c - ((S*m)/P)
+}
+uniroot(S_eq, c(0,20))$root
+
+
+# Quiz 2 ----
+
+# ekv. 1: dIdt = alfa * I - beta * I * S
+# ekv. 2: dSdt = delta * I * S - gamma * S
+
+LV_fun <- function(times, # Intervall
+                   y,     # Begynnelsevärden
+                   parms  # Andra värden
+) {
+  alfa <- parms["alfa"]
+  beta <- parms["beta"]
+  delta <- parms["delta"]
+  gamma <- parms["gamma"]
+  
+  I <- y["I"]
+  S <- y["S"]
+  
+  dIdt <- alfa * I - beta * I * S
+  dSdt <- delta * I * S - gamma * S
+  
+  result <- list(
+    dIdt,
+    dSdt
+  )
+  return(result)
+}
+
+time_span_LV <- seq(
+  0,
+  30,
+  by = 1
+)
+
+init_LV <- c(
+  I = 200,
+  S = 10
+)
+
+params_LV <- c(
+  alfa = 2.5,
+  brata = 0.15,
+  delta = 0.02,
+  gamma = 1.5
+)
+
+sol_LV <- ode(
+  y = init_LV,           # Begynnelsevärden
+  times = time_span_LV,  # Intervall
+  func = LV_fun,         # Funktionen
+  parms = params_LV,     # Andra värden
+  method = "rk4"         # Runge-Kutta version 4
+)
+sol_LV <- as.data.frame(sol_LV)
