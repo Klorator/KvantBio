43bec807335775641146f52844b62074a230493a
klorator <rasmus.hammar@gmx.com>
2024-03-14 13:09:46 +0100

    patch for bootstrap

---
+++ Tenta MatStat 2022.R
@@ -0,0 +1,33 @@
+# MatStat tenta 2022
+
+# Statistik ----
+
+## Fråga 2 ----
+nektarin <- read.table(
+  file = "Data nektarin.csv",
+  sep = ";",
+  dec = ",",
+  header = T
+)
+
+# Regression
+nekt_lm <- lm(
+  ohms ~ juice,
+  data = nektarin
+)
+
+# Diagnostics
+model_diagnostic <- function(modell) {
+  par(mfrow = c(1,2))
+  plot(modell, which = 1)
+  plot(modell, which = 2)
+  par(mfrow = c(1,1))
+}
+model_diagnostic(nekt_lm)
+# Andragradare?
+# qq-plot looks good
+
+# Gör bootstrap!
+nekt_boot <- boot.pval::boot_summary(nekt_lm)
+
+print(paste("Anpassad linje är: y =",nekt_boot$Estimate[2],"* x +",nekt_boot$Estimate[1]))
